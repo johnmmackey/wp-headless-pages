4f0b04a41114fea72d23701836384836dd7130c6
John Mackey <john.m.mackey@gmail.com>
2020-12-17 22:22:33 -0500

    Initial Commit

---
+++ headless-pages.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Headless Pages
+Plugin URI:  http://www.52west.net
+Description: Disables the default WP editor and other unneeded features on the Page Edit screen for use in headless CMS applications
+Version:     1.0.0
+Author:      John Mackey
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html 
+*/
+
+/**
+ * Hide the Page and Post Content Editor - Gutenberg, as well as other fields
+ * not used in headless mode
+ */
+add_action( 'init', function() {
+        remove_post_type_support( 'page', 'editor' );
+        remove_post_type_support( 'page', 'excerpt' );
+        remove_post_type_support( 'page', 'trackbacks' );
+        remove_post_type_support( 'page', 'comments' );
+}, 99);
+?>
+
